dfa730b9709cc5dec996b9259670ff35ba53c135
Nathan Keller <keller.nathan@gmail.com>
2019-11-02 12:44:42 -0600

    create offset class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,8 +1,21 @@
-class Enigma
+require 'date'
 
+class Enigma
+  attr_reader :char_set
+
+  def initialize
+    @char_set = ("a".."z").to_a << " "
+    @key = rand(10000..99999)
+
+  end
+
+  def encrypt(message, key, date)
+  end
 
 end
 
+# t = Time.now
+# date = [t.day, t.month, t.year]
 
 #
 # CIPHER = [*?A..?Z], [*?a..?z]
