5ce2ef68238e8c9fa9a906c633b40a3889b23bd6
Nathan Keller <keller.nathan@gmail.com>
2019-11-02 12:45:10 -0600

    create offset_test

---
+++ test/offset_test.rb
@@ -0,0 +1,42 @@
+require_relative './test_helper'
+require 'date'
+require 'mocha/minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/enigma'
+require_relative '../lib/offset'
+
+class OffsetTest < Minitest::Test
+
+  def setup
+    @offset = Offset.new
+  end
+
+  def test_it_exists
+    assert_instance_of Offset, @offset
+  end
+#need to change all these to mocks
+  def test_collect_day
+    assert_equal "02", @offset.collect_day
+  end
+  #
+  def test_collect_month
+    assert_equal "11", @offset.collect_month
+  end
+
+  def test_collect_year
+    assert_equal "19", @offset.collect_year
+  end
+
+  def test_combine
+    assert_equal "021119", @offset.combine
+  end
+
+  def test_square
+    assert_equal "446012161", @offset.square_date
+  end
+
+  # def test_take_last_4
+  #   assert_equal ""
+  # end
+end
